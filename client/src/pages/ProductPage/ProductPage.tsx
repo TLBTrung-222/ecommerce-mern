1064be10885c2726b0709996c1711feb401c066c
TrungTLB <baotrung2852003@gmail.com>
2024-09-15 11:25:57 +0700

    final step to deploy

--- client/src/pages/ProductPage/ProductPage.tsx
+++ client/src/pages/ProductPage/ProductPage.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 function ProductPage() {
-    return <div>ProductPage</div>;
+    return <div>ProductPage</div>
 }
 
-export default ProductPage;
+export default ProductPage
--- client/src/pages/TypeProductPage/TypeProductPage.tsx
+++ client/src/pages/TypeProductPage/TypeProductPage.tsx
@@ -1,30 +1,29 @@
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import { styled } from "@mui/material/styles";
-import CardComponent from "~/components/CardComponent/CardComponent";
-import Pagination from "@mui/material/Pagination";
-import { Box } from "@mui/material";
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+import { styled } from '@mui/material/styles'
+import Pagination from '@mui/material/Pagination'
+import { Box } from '@mui/material'
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-}));
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+}))
 
 function TypeProductPage() {
-    const items = ["TV", "Tu lanh", "Laptop"];
+    const items = ['TV', 'Tu lanh', 'Laptop']
 
     return (
         <Box
             sx={{
-                minHeight: "100vh",
-                display: "flex",
-                flexDirection: "column",
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column'
             }}
         >
-            <Grid container spacing={2} sx={{ flexGrow: 1, p: "10px 20px" }}>
+            <Grid container spacing={2} sx={{ flexGrow: 1, p: '10px 20px' }}>
                 {/* Type list as nav bar */}
                 <Grid gap={1} item xs={3}>
                     {items.map((item) => (
@@ -39,23 +38,17 @@ function TypeProductPage() {
                     item
                     xs={8}
                     sx={{
-                        alignContent: "flex-start", // Align items at the start of the container
+                        alignContent: 'flex-start' // Align items at the start of the container
                     }}
-                >
-                    <CardComponent />
-                    <CardComponent />
-                    <CardComponent />
-                    <CardComponent />
-                    <CardComponent />
-                </Grid>
+                ></Grid>
             </Grid>
 
             {/* Pagination */}
-            <Box sx={{ display: "flex", justifyContent: "center", my: 4 }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', my: 4 }}>
                 <Pagination count={10} color="primary" size="large" />
             </Box>
         </Box>
-    );
+    )
 }
 
-export default TypeProductPage;
+export default TypeProductPage
